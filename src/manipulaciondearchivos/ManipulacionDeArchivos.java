46b957a8656329a548618aaf55dd6b2c7fced421
Xenon <alguirre2@gmil.com>
2023-10-10 17:37:49 +0100

    escritura de texto

--- src/manipulaciondearchivos/ManipulacionDeArchivos.java
+++ src/manipulaciondearchivos/ManipulacionDeArchivos.java
@@ -9,6 +9,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 
 /**
  *
@@ -42,7 +44,7 @@ public class ManipulacionDeArchivos {
    
 
 public static String[] fileToStringArray(String name) throws IOException{
-    
+
     File archivo;
     FileReader reader;
     BufferedReader bufer;
@@ -50,14 +52,11 @@ public static String[] fileToStringArray(String name) throws IOException{
     String[] array=null;
     int t;
     int i=0;
-    
     try{ 
-       
         // obtener el tamaño del archivo
         t=countFileLine(name);
         // con el tamaño del archivo, contruir el arrelgo 
-        array= new String[t];
-        
+        array= new String[t];     
     //  crear un apuntar al archovo fisico
     archivo = new File("C:\\archivos\\"+ name + ".txt");
     // abrir le archivo para lectura 
@@ -69,7 +68,6 @@ public static String[] fileToStringArray(String name) throws IOException{
        // System.out.println("Linea leida: "+ linea);
        array[i]=linea;
        i++;
-      
         }
      reader.close();
     } catch (Exception e){
@@ -78,11 +76,73 @@ public static String[] fileToStringArray(String name) throws IOException{
     return array;
     
 }
+public static int[] fileToIntArray(String name) throws IOException{
+    
+    File archivo;
+    FileReader reader;
+    BufferedReader bufer;
+    String linea;
+    int[] array=null;
+    int t;
+    int i=0;
+    try{ 
+        t=countFileLine(name);
+        array= new int[t];     
+    //  crear un apuntar al archovo fisico
+    archivo = new File("C:\\archivos\\"+ name + ".txt");
+    reader = new FileReader(archivo);
+    bufer = new BufferedReader(reader);
+    while((linea = bufer.readLine())!=null){
+       array[i]=Integer.parseInt(linea);
+       i++;
+        }
+     reader.close();
+    } catch (Exception e){
+        System.out.println("Error al leer el archivo" + e.toString());
+    }
+    return array;
+    
+}
+
+public static void writeFile(String name){
+   
+   
+// apuntador a un espacio fisico del disco duro
+    FileWriter archivo; 
+    //llave de acceso de escritura
+    PrintWriter writer;
+    
+    //Escribir del teclado al discoduro
+    BufferedReader bufer= new BufferedReader(new InputStreamReader(System.in));
+    String entrada;
+    char respuesta;
+    
+    try{
+        //apuntador al archivo a crear
+        archivo = new FileWriter("C:\\archivos\\"+name+".txt");
+        writer = new PrintWriter(archivo);
+        do{
+            System.out.println("Escribe el contenido del archivo");
+            entrada=bufer.readLine();
+            // guardar lo recuperado dle telcado al archivo
+            writer.println(entrada);
+            System.out.println("Escribe la letra X para detener, cualquier otra tecla llena el teclado");
+            entrada=bufer.readLine();
+            respuesta= entrada.charAt(0);
+        } while (respuesta !='x');
+        archivo.close();
+    } catch (Exception e){
+        System.out.println("Error al escribir el Archivo"+ e.toString);
+    }
+    
+}
+
     public static void main(String[] args) throws IOException {
         BufferedReader bufer= new BufferedReader(new InputStreamReader(System.in));
         
         String fileName;   
         String[] juegos;
+        int[] num;
         
         System.out.println("lectura del un archoivo de texto");
         System.out.println("Escribe el nombre del archivo");
@@ -92,6 +152,20 @@ public static String[] fileToStringArray(String name) throws IOException{
         for(String unlinea : juegos){
             System.out.println(unlinea);
         }
+        System.out.println("lectura de datos numericos");
+        System.out.println("Escribe el nombre del archivo");
+        fileName= bufer.readLine();
+        num=fileToIntArray(fileName);
+        System.out.println("contenido del array de numeros");
+        for(int numi : num){
+        System.out.println(numi);
+    }
+        
+        System.out.println("crear un archivo de texto");
+        System.out.println("ingresa el nombre del archivo");
+        fileName= bufer.readLine();
+        writeFile(fileName);
+        
     }
     
 }
